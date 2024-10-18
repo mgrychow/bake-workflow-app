548bbe8c5503d1d6b3f309abd00ae477aaa92493
Grychowski, Maciej <maciej.grychowski@intel.com>
2024-10-18 19:01:36 +0200

    check if hcl file exists, if not add to build command

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker buildx bake -f docker-bake.hcl -f docker-bake-override.hcl
+if [ $# -gt 0 ]; then
+    ARG=$1
+    docker buildx bake --print -f docker-bake.hcl -f docker-bake-override.hcl -f $ARG
+    docker buildx bake -f docker-bake.hcl -f docker-bake-override.hcl -f $ARG
+else
+    docker buildx bake --print -f docker-bake.hcl -f docker-bake-override.hcl
+    docker buildx bake -f docker-bake.hcl -f docker-bake-override.hcl
+fi
